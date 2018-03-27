65f479fd7d5328c562d7e97cf479e0f10db3dfcf
foobert <0x002a@gmail.com>
2018-03-27 10:50:02 +0200

    cleanup files

--- src/s3.js
+++
@@ -1,9 +0,0 @@
-const AWS = require("aws-sdk");
-
-const config = {
-  s3ForcePathStyle: true,
-  accessKeyId: "ACCESS_KEY_ID",
-  secretAccessKey: "SECRET_ACCESS_KEY",
-  endpoint: new AWS.Endpoint("http://localhost:4572")
-};
-module.exports = new AWS.S3(config);
